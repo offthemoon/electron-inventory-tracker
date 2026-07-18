dcb0247b03c3041d5c463adafdd44e1fbd8203b8
Roy Ibarra <royibarra2@gmail.com>
2026-07-18 10:54:18 -0400

    feat: added feature to allow users to add tracking / order #'s, also allow to set status of package

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -42,8 +42,14 @@ function App(): React.JSX.Element {
 
   const [set_name, set_set_name] = useState('');
   const [tracking_id, set_tracking_id] = useState('');
-
   const [retailer, set_retailer] = useState('Pokémon Center');
+
+  const [order_id, set_order_id] = useState('');
+
+  const [quantity, set_quantity] = useState('0');
+
+
+  const [delivered,set_delivered] = useState('false');
 
 
 
@@ -53,16 +59,18 @@ function App(): React.JSX.Element {
 
     <>
     <div>
-      <h1> Hello World </h1> 
+      <h1> Order Verfier </h1> 
     </div>
 
 
     <section className='All Information'> 
+      {/** Potentially can make this also a select but with some many options do not want to list like i did for retailer.  */}
+
       <div>
         <label> Please Enter Set Name  -- </label>
         <input value={set_name}
         onChange={(event) => set_set_name(event.target.value)}    
-        placeholder='Enter Tracking ID (UPS/FedEx/USPS'
+        placeholder='Pitch Black, Destin Rivals...'
         /> 
       </div>
 
@@ -76,10 +84,60 @@ function App(): React.JSX.Element {
            <option value = 'Walmart'> Walmart </option>
            <option value = 'N/A'>  N/A </option>
           </select>
+      </div>
 
-          
+
+      <div> 
+          {/* <label> Enter Quantity </label>
+
+          <input value={quantity}
+          onChange={event =>{
+
+            const value = event.target.value; //Grab the current value. 
+            if(value === '' || /^\d+$/.test(value))
+            {
+              set_quantity(value);
+            }
+
+          }
+        }
+          />  */}
 
       </div>
+
+
+      <div>
+            <label> Enter Tracking Number </label>
+
+            <input value={tracking_id}
+            onChange={(event) => set_tracking_id(event.target.value)}
+            placeholder='FedEx/UPS...'
+            /> 
+      </div>
+
+      <div>
+          <label> Enter Retailer Order ID </label>
+          <input value={order_id}
+          onChange={(event) => set_order_id(event.target.value)}
+          placeholder='P03.....'
+          />
+      </div>
+
+      <div> 
+        <label> Set Status of Order: </label>
+            <select value={delivered}
+            onChange={event => set_delivered(event.target.value)}
+            > 
+                <option value='true'> Delivered </option>
+                <option value='false'> Not Delivered </option>
+            </select>
+
+      </div>
+
+    
+
+
+
 
     </section>
         
